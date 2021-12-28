e9f68cb76d6baec550cd3964bfc80cc417513559
Rashad Philizaire <rashadphilizaire@gmail.com>
2021-12-27 20:09:25 -0500

    create base db schema
    Signed-off-by: Rashad Philizaire <rashadphilizaire@gmail.com>

---
+++ swe-track/src/server/schema.sql
@@ -0,0 +1,28 @@
+CREATE TYPE membership_status AS ENUM ('free', 'tier1', 'tier2');
+
+CREATE TABLE users (
+  uid SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
+  email VARCHAR(255) UNIQUE,
+  email_verified BOOLEAN,
+  date_created DATE,
+  last_login DATE,
+  membership membership_status
+);
+
+CREATE TABLE companies (
+  cid SERIAL PRIMARY KEY,
+  company_name VARCHAR(255) UNIQUE,
+  website_link VARCHAR(255),
+  application_link VARCHAR(255),
+  intern_salary INT
+);
+
+CREATE TYPE application_status AS ENUM ('not open', 'rejected', 'applied', 'online assesement', 'interview rounds', 'final round', 'offer');
+
+CREATE TABLE user_companies (
+  user_id INT references users(uid),
+  company_id INT references companies(cid),
+  user_status application_status,
+  date_applied DATE
+);
