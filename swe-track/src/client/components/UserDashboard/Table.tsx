72792cc2405ba47afb794d74d4355bdfdba15297
Rashad Philizaire <rashadphilizaire@gmail.com>
2022-01-10 15:07:44 -0500

    sort visual change on frontend
    Signed-off-by: Rashad Philizaire <rashadphilizaire@gmail.com>

--- swe-track/src/client/components/UserDashboard/Table.tsx
+++ swe-track/src/client/components/UserDashboard/Table.tsx
@@ -5,22 +5,89 @@ import {
 } from '@heroicons/react/solid'
 import { useState } from 'react'
 
+const sorts = new Map<number, string>([
+  [0, ''],
+  [1, 'asc'],
+  [2, 'desc']
+])
+
+type SortIconProps = {
+  className: string
+  sort: number
+  onClick: () => void
+}
+function SortIcon({ className, sort, onClick }: SortIconProps) {
+  switch (sort) {
+    case 0:
+      return (
+        <SelectorIcon
+          className={className + ' text-gray-500'}
+          onClick={onClick}
+        />
+      )
+    case 1:
+      return <ArrowSmUpIcon className={className} onClick={onClick} />
+    case 2:
+      return <ArrowSmDownIcon className={className} onClick={onClick} />
+    default:
+      return <div></div>
+  }
+}
+type HeaderProps = {
+  className: string
+  title: string
+  sort: number
+  setSort: (value: number) => void
+}
+
+function Header({ className, title, sort, setSort }: HeaderProps) {
+  return (
+    <th className={className}>
+      <div className="inline-flex">
+        {title}
+        <SortIcon
+          className="pl-1 w-5 h-5 hover:cursor-pointer"
+          sort={sort}
+          onClick={() => setSort(sort == 2 ? 0 : sort + 1)}
+        />
+      </div>
+    </th>
+  )
+}
 export default function Table({ entries }: any) {
   const [companySort, setCompanySort] = useState(0)
+  const [statusSort, setStatusSort] = useState(0)
+  const [dateSort, setDateSort] = useState(0)
+  const [problemSort, setProblemSort] = useState(0)
   return (
     <div className="my-6 bg-white rounded shadow-md dark:bg-gray-700">
       <table className="w-full table-auto min-w-max">
         <thead>
           <tr className="text-sm leading-normal text-black uppercase bg-white border-b border-black dark:bg-gray-800 dark:text-white">
-            <th className="px-6 py-3 text-left">
-              <div className="inline-flex">
-                Company
-                <SelectorIcon className="w-5 h-5 hover:cursor-pointer" />
-              </div>
-            </th>
-            <th className="px-6 py-3 text-center">Status</th>
-            <th className="px-6 py-3 text-left">Date Applied</th>
-            <th className="px-6 py-3 text-center">LC Problems</th>
+            <Header
+              className="px-6 py-3 text-left"
+              title={'Company'}
+              sort={companySort}
+              setSort={setCompanySort}
+            />
+            <Header
+              className="px-6 py-3 text-center"
+              title={'Status'}
+              sort={statusSort}
+              setSort={setStatusSort}
+            />
+            <Header
+              className="px-6 py-3 text-left"
+              title={'Date Applied'}
+              sort={dateSort}
+              setSort={setDateSort}
+            />
+            <Header
+              className="px-6 py-3 text-center"
+              title={'LC Problems'}
+              sort={problemSort}
+              setSort={setProblemSort}
+            />
           </tr>
           {entries}
         </thead>
